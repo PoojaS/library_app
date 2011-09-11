84eef7752b304bf9c224809f73b1fe21482ddb7e
Pooja <poojas2406@gmail.com>
2011-09-11 19:34:43 +0530

    Modelled book.rb by writing validation tests in the book_spec.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ActiveRecord::Base
+  attr_accessible :title, :author ,:no_of_copies_available , :user_id
+  validates(:title,:presence => true)
+  validates(:author,:presence => true)
   belongs_to :user
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Book do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before (:each) do
+    @book_attr= {:title => "ExampleBook",:author =>"ExampleAuthor",:no_of_copies_available=>1, :user_id => 1}
+  end
+
+  it "should create a new row with specified attributes" do
+    Book.create!(@book_attr)
+  end
+
+  it "should require a title" do
+    no_title_book = Book.new(@book_attr.merge(:title => ""))
+    no_title_book.should_not be_valid
+  end
+
+  it "should require an author" do
+    no_author_book=Book.new(@book_attr.merge(:author => ""))
+    no_author_book.should_not be_valid
+  end
+
 end
