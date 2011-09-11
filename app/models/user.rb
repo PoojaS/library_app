59284c62fc36020e05af07b7107ae8d07d9ef74a
Pooja <poojas2406@gmail.com>
2011-09-12 01:31:09 +0530

    Added the user spec and validated the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessible :first_name, :last_name
+  validates(:first_name,:presence => true)
+  validates(:last_name,:presence => true)
   has_many :books
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+    before (:each) do
+    @user_attr= {:first_name => "Pooja",:last_name => "Subramanian"}
+  end
+
+  it "should create a new row with specified attributes" do
+    User.create!(@user_attr)
+  end
+
+  it "should require a Fisrt name" do
+    no_first_name_attr = User.new(@user_attr.merge(:first_name => ""))
+    no_first_name_attr.should_not be_valid
+  end
+
+  it "should require a last name" do
+    no_last_name_attr=User.new(@user_attr.merge(:last_name => ""))
+    no_last_name_attr.should_not be_valid
+  end
 end
